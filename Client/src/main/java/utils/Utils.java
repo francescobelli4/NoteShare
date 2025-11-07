82b0b5a4753ef5fb78639e0bfbc0ef6d035c1a69
Francesco <francesco8995@gmail.com>
2025-11-07 13:14:41 +0100

    SonarCloud

--- Client/src/main/java/utils/Utils.java
+++ Client/src/main/java/utils/Utils.java
@@ -3,12 +3,13 @@ package utils;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 import static utils.PathUtils.getOSLocalPath;
 
 
 public class Utils {
+
+    private Utils() {}
 
     public static void createUserSubfolders() {
         File dir = new File(getOSLocalPath() + "MyNotes");
@@ -21,14 +22,8 @@ public class Utils {
      */
     public static void saveAccessToken(String token) {
 
-        File tokenFile = new File(getOSLocalPath() + "token.txt");
-
-        try {
-            tokenFile.createNewFile();
-
-            FileWriter fileWriter = new FileWriter(getOSLocalPath() + "token.txt");
+        try (FileWriter fileWriter = new FileWriter(getOSLocalPath() + "token.txt")) {
             fileWriter.write(token);
-            fileWriter.close();
         } catch (IOException e) {
             // TODO
             e.printStackTrace();
