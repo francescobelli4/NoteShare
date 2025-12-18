3316a72a972346f3bf6957d362e635f2f7a165bf
Francesco <francesco8995@gmail.com>
2025-12-18 17:11:57 +0100

    Fixing smells

--- Client/src/main/java/views/View.java
+++ Client/src/main/java/views/View.java
@@ -9,5 +9,8 @@ public interface View {
     void init();
     Parent getRoot();
     Page getPage();
+    // SonarCloud finds a smell here for the generic type, but actually this is needed
+    // for the architecture.
+    @SuppressWarnings("java:S1452")
     GraphicsController<? extends View> getGraphicsController();
 }
