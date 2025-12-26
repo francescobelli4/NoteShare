a00d096f40e2ee9d2e49f2fef45621b6c344e860
francesco <francesco8995@gmail.com>
2025-12-26 16:40:56 +0100

    Tests

---
+++ Shared/src/test/java/communication/TestSocketMessageTypeAdapter.java
@@ -0,0 +1,16 @@
+package communication;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestSocketMessageTypeAdapter {
+
+    @Test
+    void write() {
+    }
+
+    @Test
+    void read() {
+    }
+}
