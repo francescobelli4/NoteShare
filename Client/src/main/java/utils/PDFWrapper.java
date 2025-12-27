244d3875870c87084a739a0d00d90a390bebbe21
francesco <francesco8995@gmail.com>
2025-12-27 18:10:55 +0100

    Tests

--- Client/src/main/java/utils/PDFWrapper.java
+++ Client/src/main/java/utils/PDFWrapper.java
@@ -1,27 +1,42 @@
 package utils;
 
 import com.google.gson.Gson;
+
 import org.apache.pdfbox.Loader;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
+
 import org.apache.pdfbox.pdmodel.PDPage;
 
 import java.io.File;
+
 import java.io.IOException;
 
 /**
+
  * This is a wrapper class that should define a pdf file better.
+
  * Every time the user adds a note and chooses a pdf file, a wrapper for that file should be created
+
  * and it should add some operation to a pdf file:
+
  * it should allow the creation of an image from a pdf page and it should allow to create a PDFConfig, which
+
  * is a class that can be encoded to json and represents some important data for that pdf file that are useful
+
  * to display the pdf and should not be re-calculated every time the user wants to see the note.
+
  *
+
  * In demo mode, the config file is not actually saved in a persistent file.
+
  */
+
 public class PDFWrapper {
 
     /** Needed to convert from PDF's measures to pixels */
     private static final float PDF_POINTS_PER_INCH = 72f;
+
     /** Used to specify the image quality and calculate the image dimensions in pixel*/
     private static final float IMAGE_DPI = 150f;
 
@@ -32,20 +47,23 @@ public class PDFWrapper {
      * Base constructor
      *
      * This constructor should create a new PDFWrapper and set up its config class.
-     *
      * @param pdfFile the PDF file
      */
     public PDFWrapper(File pdfFile) {
+
         pdfConfig = new PDFConfig();
         pdfConfig.setPdfFile(pdfFile.getPath());
 
         try {
+
             PDDocument pdfDocument = Loader.loadPDF(pdfFile);
+
             pdfConfig.setNumberOfPages(pdfDocument.getNumberOfPages());
 
             for (PDPage page : pdfDocument.getPages()) {
 
                 float pageWidthWithDPI = (page.getBBox().getWidth() / PDF_POINTS_PER_INCH) * IMAGE_DPI;
+
                 float pageHeightWithDPI = (page.getBBox().getHeight() / PDF_POINTS_PER_INCH) * IMAGE_DPI;
 
                 if (pageWidthWithDPI > pdfConfig.getMaxWidth()) {
@@ -76,12 +94,12 @@ public class PDFWrapper {
     /**
      * This function should convert a pdf page to a image (which is contained in PageToImageResponse, together
      * with the page id).
-     *
      * More info about this conversion in PDFToImage class :D
      *
      * @param pageId the id of the page that should be converted
      * @return a response that contains page id and the image
      */
+
     public PDFToImage.PageToImageResponse pageToImage(int pageId) {
         return PDFToImage.requestPageToImage(pdfConfig.getPdfFilePath(), pageId, (int)IMAGE_DPI);
     }
@@ -94,7 +112,9 @@ public class PDFWrapper {
      * This class represents a config for every pdf wrapper that can be encoded and decoded.
      * This should be used to keep trace of the data needed to display the pdf.
      */
+
     public class PDFConfig {
+
         private float maxWidth;
         private float maxHeight;
         private String pdfFilePath;
@@ -137,5 +157,7 @@ public class PDFWrapper {
         public int getNumberOfPages() {
             return numberOfPages;
         }
+
     }
+
 }
---
+++ Client/src/test/java/utils/TestPDFWrapper.java
@@ -0,0 +1,61 @@
+package utils;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageTree;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.MockedStatic;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class TestPDFWrapper {
+
+    PDFWrapper pdfWrapper;
+
+    @BeforeEach
+    void setup(@TempDir Path tempDir) {
+
+        PDDocument mockedPDDocument = mock(PDDocument.class);
+        PDPage mockedPage = mock(PDPage.class);
+        PDRectangle mockedBBox = mock(PDRectangle.class);
+        when(mockedBBox.getWidth()).thenReturn(100f);
+        when(mockedBBox.getHeight()).thenReturn(100f);
+        when(mockedPage.getBBox()).thenReturn(mockedBBox);
+
+        PDPageTree mockedPages = mock(PDPageTree.class);
+        mockedPages.add(mockedPage);
+
+        when(mockedPDDocument.getPages()).thenReturn(mockedPages);
+        when(mockedPDDocument.getNumberOfPages()).thenReturn(1);
+        when(mockedPages.iterator()).thenReturn(List.of(mockedPage).iterator());
+
+        Path filePath = tempDir.resolve("test.txt");
+        File fileRealeTemporaneo = filePath.toFile();
+
+        try (MockedStatic<Loader> staticLoader = mockStatic(Loader.class)) {
+            staticLoader.when(() -> Loader.loadPDF(any(File.class))).thenReturn(mockedPDDocument);
+
+            pdfWrapper = new PDFWrapper(fileRealeTemporaneo);
+        }
+    }
+
+    @Test
+    void pageToImage() {
+
+    }
+
+    @Test
+    void getPdfConfig() {
+        assertNotNull(pdfWrapper.getPdfConfig());
+    }
+}
